5310acc318ff9adf956f2b3c3794ff39f946f4aa
Jan <31709392+cuellius@users.noreply.github.com>
2018-01-16 23:09:41 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 CFLAGS=$(python3-config --includes)
 LDFLAGS=$(python3-config --ldflags)
-g++ -std=c++14 -O2 -Wall MS++/cMS.cpp MS++/StringUtils.cpp MS++/ModuleSystem.cpp MS++/CPyObject.cpp MS++/OptUtils.cpp -o MS++-linux $CFLAGS $LDFLAGS 
-chmod 755 MS++-linux
+g++ -std=c++14 -O2 -Wall cMS.cpp StringUtils.cpp ModuleSystem.cpp CPyObject.cpp OptUtils.cpp -o ms-pp-linux $CFLAGS $LDFLAGS 
+chmod 755 ms-pp-linux
